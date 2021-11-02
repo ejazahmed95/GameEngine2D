3b544208c2f61ad5a2eae00d9d5a70e114e51ffd
Ejaz Ahmed <ejazahmed0195@gmail.com>
2021-11-02 01:23:12 -0600

    Implementation for MakeSentence

---
+++ IndependentCodes/src/MakeSentence.cpp
@@ -0,0 +1,91 @@
+#include <cassert>
+#include <conio.h>
+
+#include "AllCodes.h"
+#include <iostream>
+
+char* readStringFromCIn() {
+    char a[20];
+    // std::cin >> a;
+    int len = 0;
+	while (len <= 20) {
+		char c;
+		c = std::getchar();
+		if (c != '\n' && c != '\0') {
+			a[len++] = c;
+		} else break;
+	}
+
+    // char* str = (char*)malloc((len+1) * sizeof(char));
+	// if (len == 2 && a[0]=='q') return nullptr;
+	if (len == 0) return nullptr;
+	len++;
+    char* str = new char[len];
+    memcpy(str, a, len - 1);
+    str[len-1] = '\0';
+    return str;
+}
+
+int len(const char* word) {
+	int len = 0;
+	while (word[len] != '\n' && word[len]!='\0') len++;
+	return len;
+}
+
+char* MakeSentence(const char** arr) {
+	int totalLength = 0;
+	for(int i=0;;i++) {
+		const char* word = arr[i];
+		if (word == nullptr) break;
+		totalLength += (len(word) + 1);
+	}
+	totalLength++;
+
+	char* str = static_cast<char*>(malloc(totalLength * sizeof(char)));
+	assert(str);
+	str[totalLength - 1] = '\0';
+
+	int currIndex = 0;
+	for (int i = 0;; i++) {
+		const char* word = arr[i];
+		if (word == nullptr) break;
+		memcpy(str + currIndex, word, len(word));
+		currIndex += len(word);
+		str[currIndex++] = ' ';
+		// std::cout << str << std::endl;
+	}
+	if(currIndex > 0) str[currIndex-1] = '.';
+	return str;
+}
+
+
+void TestMakeSentence() {
+	const char* strings[] = {
+		"This",
+		"is",
+		"a",
+		"test",
+		nullptr
+	};
+
+	char** words = new char*[20];
+	int wordsLen = 0;
+	char* currWord;
+	do {
+		std::cout << "Enter the next word, enter just q to quit: ";
+		currWord = readStringFromCIn();
+		words[wordsLen++] = currWord;
+	} while (currWord != nullptr && wordsLen < 20);
+
+	std::cout << std::endl;
+	char* pSentence = MakeSentence(const_cast<const char**>(words));
+
+	printf("The Sentence is: %s\n", pSentence);
+
+	
+	free(pSentence);
+
+#if defined(_DEBUG)
+	_CrtDumpMemoryLeaks();
+#endif // _DEBUG
+}
